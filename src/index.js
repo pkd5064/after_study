f56463988d5f03826a601aeec9142cca6f769ce7
pkd5064 <pkd5064@gmail.com>
2019-09-09 16:12:09 +0900

    보드유저

--- src/index.js
+++ src/index.js
@@ -1,49 +1,16 @@
 const express = require("express");
 const app = express();
+const user_router = require("./route/users");
+const board_router = require("./route/boards");
 
-let users = [];
-let user = null;
-app.use(express.json());
+app.use(express.json());        
 app.use(express.urlencoded({ extended: true}));
+app.use("/users", user_route);
+app.use("/boards", board_route);
 
 app.listen(3000);
 
-app.get("/users", (req, res) => {
-    if(user){
-        res.send("user" +user.name+"get");
-
-    }else{
-        res.send("유저가 존재하지 않습니다.");
-    }
+app.get("/", (req, res) => {
+        res.send("hello world");
 });
-
-app.get("/users/:id", (req, res) => {
-    if(user&&user.id == req.params.id){
-        res.send("user "+user.name+" get");
-    }else{
-        res.send("user id "+req.params.id+" 가 존재하지 않습니다.");
-    }
-});
-
-app.post("/users", (req, res) => {
-    user = req.body;
-    res.send("user add "+user.name);
-});
-
-app.post("/users/:id", (req, res) => {
-    if(user && user.id == req.params.id){
-        user.name = req.body.name;
-        res.send("user name"+user.name+" 으로 변경");
-    }else{
-        res.send("user id "+req.params.id+" 가 존재하지 않습니다.");
-    }
-});
-
-app.delete("/users/:id", (req,res) =>{
-    if(user && user.id == req.params.id){
-        user = null;
-        res.send("user id"+req.params.id+"삭제")
-    }else{
-        res.send("user id"+req.params.id+"가 존재하지 않습니다.")
-}
-});
+ 
---
+++ src/route/boards.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.send("get all");
+});
+
+router.get("/:id", (req,res) => {
+    res.send("get id");
+});
+
+router.post("/", (req,res) => {
+    res.send("post");
+});
+
+router.put("/:id", (req,res) => {
+    res.send("put");
+});
+
+router.delete("/:id", (req,res) => {
+    res.send("delete");
+});
+
+module.exports = router;
